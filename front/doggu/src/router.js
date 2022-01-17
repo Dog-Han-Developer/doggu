534ca3a6ad2e09b3e0678b39968ee2bc6f91f229
Hwang-Sangjin <52124805+Hwang-Sangjin@users.noreply.github.com>
2022-01-17 22:42:33 +0900

    react router

---
+++ front/doggu/src/router.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./router/login";
+import Signup from "./router/signup";
+
+function router() {
+    return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+        <Route path="/signup" element={<Signup />}/>
+      </Routes>
+    </Router>
+    )
+}
+
+export default router;
---
+++ front/doggu/src/router/signup.js
@@ -0,0 +1,6 @@
+function Signup(){
+    
+    return <h1>Signup</h1>
+}
+
+export default Signup
