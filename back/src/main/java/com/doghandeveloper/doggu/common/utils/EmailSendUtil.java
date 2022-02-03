0a289728e0e1db956867eb18088e7533066a1d60
LeeA0 <lay0711@naver.com>
2022-02-04 02:56:18 +0900

    DOG-26 / feat: 이메일 전송을 위한 Email Util 개발

---
+++ back/src/main/java/com/doghandeveloper/doggu/common/utils/EmailSendUtil.java
@@ -0,0 +1,21 @@
+package com.doghandeveloper.doggu.common.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class EmailSendUtil {
+
+    private final JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+}
