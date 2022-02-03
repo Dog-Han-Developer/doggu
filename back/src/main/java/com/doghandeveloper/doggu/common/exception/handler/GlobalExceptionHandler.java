c1b0cc9d7e33c16ff20ecf869d9d8cd63471438a
LeeA0 <lay0711@naver.com>
2022-02-04 03:04:34 +0900

    DOG-26 / feat: 인증번호 이메일 전송과 관련된 에러 핸들링

--- back/src/main/java/com/doghandeveloper/doggu/common/exception/handler/GlobalExceptionHandler.java
+++ back/src/main/java/com/doghandeveloper/doggu/common/exception/handler/GlobalExceptionHandler.java
@@ -1,7 +1,28 @@
 package com.doghandeveloper.doggu.common.exception.handler;
 
+import com.doghandeveloper.doggu.common.exception.dto.ErrorCode;
+import com.doghandeveloper.doggu.common.exception.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    private ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
+        log.error("MethodArgumentNotValidException: {}", ErrorCode.INVALID_INPUT_VALUE.getMessage());
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(MailException.class)
+    private ResponseEntity<ErrorResponse> handleMailException(MailException e) {
+        ErrorResponse response = ErrorResponse.of(ErrorCode.EMAIL_SEND_FAIL);
+        log.error("MailException: {}", ErrorCode.EMAIL_SEND_FAIL.getMessage());
+        return ResponseEntity.internalServerError().body(response);
+    }
 }
