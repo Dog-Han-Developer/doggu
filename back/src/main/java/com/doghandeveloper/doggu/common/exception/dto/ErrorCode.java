09079e8b3751ad77ffc14a8281b54fb1dd83a789
LeeA0 <lay0711@naver.com>
2022-02-04 02:54:35 +0900

    DOG-33 / feat: Error 응답 및 Error Code 객체 개발

--- back/src/main/java/com/doghandeveloper/doggu/common/exception/dto/ErrorCode.java
+++ back/src/main/java/com/doghandeveloper/doggu/common/exception/dto/ErrorCode.java
@@ -1,4 +1,21 @@
 package com.doghandeveloper.doggu.common.exception.dto;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public enum ErrorCode {
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST.value(), "EN_001", "올바르지 않은 요청 값입니다."),
+
+    EMAIL_SEND_FAIL(HttpStatus.INTERNAL_SERVER_ERROR.value(), "ES_001", "인증번호 이메일 전송이 실패했습니다.")
+    ;
+    private final int status;
+    private final String code;
+    private final String message;
+
+    ErrorCode(int status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
 }
