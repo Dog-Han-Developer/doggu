b64b8e01a6897ae0015b955d17632bad9508d30f
LeeA0 <lay0711@naver.com>
2022-02-04 02:55:35 +0900

    DOG-26 / feat: 인증 코드 저장을 위한 Redis Util 개발

--- back/src/main/java/com/doghandeveloper/doggu/common/utils/RedisUtil.java
+++ back/src/main/java/com/doghandeveloper/doggu/common/utils/RedisUtil.java
@@ -1,7 +1,20 @@
 package com.doghandeveloper.doggu.common.utils;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
+@RequiredArgsConstructor
 @Service
 public class RedisUtil {
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public void setDataExpire(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
 }
