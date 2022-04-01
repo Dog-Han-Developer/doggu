011912fc9b70f058e40cbbfa1c1ef85d63969dc0
at8in <at8in@naver.com>
2022-04-01 23:21:15 +0900

    DOG-111 / fix: 버튼 컴포넌트 onClick 추가 및 수정

--- front/doggu/src/components/button/signup-button.jsx
+++ front/doggu/src/components/button/signup-button.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const SignupButton = ({ title }) => {
+const SignupButton = ({ title, onClick }) => {
   return (
-    <button className="p-2 bg-brown-800 w-full rounded-lg text-white text-sm hover:bg-white/20">
+    <button
+      className="p-2 bg-brown-800 w-full rounded-lg text-white text-sm hover:bg-white/20"
+      onClick={onClick}
+    >
       {title}
     </button>
   );
