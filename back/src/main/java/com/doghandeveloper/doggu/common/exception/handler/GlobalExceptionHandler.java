7f1b5fcc82d543a0ef1f92484c9c88b28c237f98
may.ple <may.ple@kakaocorp.com>
2022-05-22 19:13:51 +0900

    fix: 메일 전송 관련 에러 메시지 잘 보이도록 변경

--- back/src/main/java/com/doghandeveloper/doggu/common/exception/handler/GlobalExceptionHandler.java
+++ back/src/main/java/com/doghandeveloper/doggu/common/exception/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.doghandeveloper.doggu.common.exception.handler;
 
 import com.doghandeveloper.doggu.common.exception.dto.ErrorCode;
 import com.doghandeveloper.doggu.common.exception.dto.ErrorResponse;
+import java.security.InvalidParameterException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     private ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
@@ -21,8 +23,15 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MailException.class)
     private ResponseEntity<ErrorResponse> handleMailException(MailException e) {
-        ErrorResponse response = ErrorResponse.of(ErrorCode.EMAIL_SEND_FAIL);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.EMAIL_SEND_FAIL, e.getMessage());
         log.error("MailException: {}", ErrorCode.EMAIL_SEND_FAIL.getMessage());
         return ResponseEntity.internalServerError().body(response);
+    }
+
+    @ExceptionHandler(InvalidParameterException.class)
+    private ResponseEntity<ErrorResponse> handleInvalidParameterException() {
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
+        log.error("InvalidParameterException: {}", ErrorCode.INVALID_INPUT_VALUE.getMessage());
+        return ResponseEntity.badRequest().body(response);
     }
 }
