e3ccb3f38adb72778e90f7de74d18f072a48a961
may.ple <may.ple@kakaocorp.com>
2022-05-22 19:07:10 +0900

    DOG-66 / test: 이메일 전송 메소드명 변경 및 인증번호 확인 Controller 테스트 코드 작성

--- back/src/test/java/com/doghandeveloper/doggu/Account/controller/docs/AccountDocumentation.java
+++ back/src/test/java/com/doghandeveloper/doggu/Account/controller/docs/AccountDocumentation.java
@@ -9,12 +9,22 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWit
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 
 public class AccountDocumentation {
-    public static RestDocumentationResultHandler emailAuthentication() {
+    public static RestDocumentationResultHandler sendAuthenticationEmail() {
         return document("accounts/email",
                 ApiDocumentUtils.getDocumentRequest(),
                 ApiDocumentUtils.getDocumentResponse(),
                 requestFields(
                         fieldWithPath("email").type(JsonFieldType.STRING).description("이메일")
                 ));
+    }
+
+    public static RestDocumentationResultHandler verifyAuthenticationCode() {
+        return document("accounts/email/verify",
+            ApiDocumentUtils.getDocumentRequest(),
+            ApiDocumentUtils.getDocumentResponse(),
+            requestFields(
+                fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                fieldWithPath("authenticationCode").type(JsonFieldType.STRING).description("인증번호")
+            ));
     }
 }
