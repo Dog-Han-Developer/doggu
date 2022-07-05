f881eccdc444bd629eb804cf8ee8f09331e20a1e
tnalsvjscl <tnalsvjscl@naver.com>
2022-07-05 23:04:32 +0900

    Dog-93 / feat: 불필요한 import 제거

--- back/src/main/java/com/doghandeveloper/doggu/Dog/dto/response/DogAuthenticationResponse.java
+++ back/src/main/java/com/doghandeveloper/doggu/Dog/dto/response/DogAuthenticationResponse.java
@@ -3,9 +3,6 @@ package com.doghandeveloper.doggu.Dog.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 @Schema(description = "반려견 인증 응답")
 @Getter
 public class DogAuthenticationResponse {
