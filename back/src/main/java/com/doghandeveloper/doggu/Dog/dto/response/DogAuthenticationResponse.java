a6ad148d87411a353e673e650024d8efc59e93b5
tnalsvjscl <tnalsvjscl@naver.com>
2022-08-31 00:45:53 +0900

    Dog- 123 / feat: DogAuthenticationResponse DTO 수정

--- back/src/main/java/com/doghandeveloper/doggu/Dog/dto/response/DogAuthenticationResponse.java
+++ back/src/main/java/com/doghandeveloper/doggu/Dog/dto/response/DogAuthenticationResponse.java
@@ -1,10 +1,15 @@
 package com.doghandeveloper.doggu.Dog.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
+
 
 @Schema(description = "반려견 인증 응답")
 @Getter
+@Setter
+@Builder
 public class DogAuthenticationResponse {
 
     @Schema(description = "동물등록증에 등록된 반려견 이름", example = "이요미")
@@ -14,6 +19,13 @@ public class DogAuthenticationResponse {
     private String dogKind;
 
     @Schema(description = "동물등록증에 등록된 반려견", example = "암컷")
-    private String dogSex;
+    private Enum dogSex;
 
+    public static DogAuthenticationResponse toDogAuthenticationResponse(String dogName, String dogKind, Enum dogSex){
+        return DogAuthenticationResponse.builder()
+                .dogName(dogName)
+                .dogKind(dogKind)
+                .dogSex(dogSex)
+                .build();
+    }
 }
