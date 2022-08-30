5f23c6053d2fa5620f24e59f3fd7ea728f8a6949
LeeA0 <lay0711@naver.com>
2022-08-30 21:45:04 +0900

    DOG-18 / feat: 견주 여부 엔티티 추가 및 createdDate 적용되게한다.

--- back/src/main/java/com/doghandeveloper/doggu/Account/domain/Account.java
+++ back/src/main/java/com/doghandeveloper/doggu/Account/domain/Account.java
@@ -2,6 +2,7 @@ package com.doghandeveloper.doggu.Account.domain;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -9,6 +10,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Account {
@@ -24,6 +26,8 @@ public class Account {
 
     private String password;
 
+    private Owner owner;
+
     @Lob
     private String introduction;
 
@@ -35,10 +39,11 @@ public class Account {
     private LocalDateTime updatedDate;
 
     @Builder
-    public Account(String email, String username, String password, String introduction) {
+    public Account(String email, String username, String password, Owner owner, String introduction) {
         this.email = email;
         this.username = username;
         this.password = password;
+        this.owner = owner;
         this.introduction = introduction;
     }
 }
