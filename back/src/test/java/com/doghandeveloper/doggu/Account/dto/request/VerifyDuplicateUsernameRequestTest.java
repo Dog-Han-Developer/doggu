f6a65fc1cc726d597595434eda15dc9e94e9bb4c
LeeA0 <lay0711@naver.com>
2022-09-13 23:11:33 +0900

    DOG-18 / test: 사용자 이름 중복 체크에 length 제한 추가에 대한 테스트 코드 추가

--- back/src/test/java/com/doghandeveloper/doggu/Account/dto/request/VerifyDuplicateUsernameRequestTest.java
+++ back/src/test/java/com/doghandeveloper/doggu/Account/dto/request/VerifyDuplicateUsernameRequestTest.java
@@ -7,12 +7,12 @@ import org.junit.jupiter.api.Test;
 
 public class VerifyDuplicateUsernameRequestTest {
 
-    private final static String PATTERN = "^[a-z0-9_\\.]*$";
+    private final static String PATTERN = "^[a-z0-9_\\.]{8,16}$";
 
     @Test
     @DisplayName("올바른 사용자 이름으로 중복 확인을 요청한다.")
     void withCorrectUsername(){
-        String username = "doggu_love1234.png";
+        String username = "doggu_1234.png";
 
         Assertions.assertThat(Pattern.matches(PATTERN, username)).isTrue();
     }
@@ -21,6 +21,14 @@ public class VerifyDuplicateUsernameRequestTest {
     @DisplayName("잘못된 형식의 사용자 이름으로 중복 확인을 요청할 수 없다.")
     void withWrongUsername(){
         String username = "doggu#love";
+
+        Assertions.assertThat(Pattern.matches(PATTERN, username)).isFalse();
+    }
+
+    @Test
+    @DisplayName("잘못된 길이의 사용자 이름으로 회원가입을 할 수 없다.")
+    void withWrongLengthUsername() {
+        String username = "doggu_love.1234567890";
 
         Assertions.assertThat(Pattern.matches(PATTERN, username)).isFalse();
     }
